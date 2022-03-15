586cf9a41afa69b65af6be63ccd4adff37073b82
Michael Simkin <michael.simkin22@gmail.com>
2022-03-15 20:23:57 +0200

    feat: set load dotevn to true on default

--- src/config/dotenv.ts
+++ src/config/dotenv.ts
@@ -1,7 +1,7 @@
 import * as dotenv from 'dotenv';
 import * as env from 'env-var';
 
-if (env.get('LOAD_DOTENV').default('false').asBool()) {
+if (env.get('LOAD_DOTENV').default('true').asBool()) {
     const dotenvPath = env.get('DOTENV_PATH').default('.env').asString();
     dotenv.config({ path: dotenvPath });
 }
